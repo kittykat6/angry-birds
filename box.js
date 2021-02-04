c09ffa8042fee4b9fc0987a1a6f9b274bfc8e6c6
kittykat6 <75424928+kittykat6@users.noreply.github.com>
2021-02-04 15:52:37 +0530

    Add files via upload

--- box.js
+++ box.js
@@ -13,10 +13,13 @@ class Box {
   }
   display(){
     var pos =this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
     rectMode(CENTER);
-push()   
- fill(255);
-    rect(pos.x, pos.y, this.width, this.height);
+    fill(255);
+    rect(0, 0, this.width, this.height);
     pop();
   }
 };
